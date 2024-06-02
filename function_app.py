4ea93aac188dde4613fd61082ac257b1dce72292
Gill Mertens <gill.mertens@student.ehb.be>
2024-06-02 11:11:36 +0200

    Feature: Increase keyworde and upadate prompt engineering

--- function_app.py
+++ function_app.py
@@ -10,21 +10,73 @@ openai_endpoint = os.getenv("OPENAI_ENDPOINT")
 
 def is_relevant_query(query):
     # Simple keyword-based approach to filter out irrelevant queries
-    keywords = ["Erasmushogeschool", "Brussel", "EHB", "programs", "admissions", "tuition", "campus", "student life"]
+    keywords = [
+    "Erasmushogeschool", "Brussel", "EHB", "Erasmus", "VUB", "universiteit", "hogeschool",
+    "lessen", "buitenland", "stage", "richting", "examens", "inschrijving", "studenten",
+    "docenten", "opleiding", "vakken", "studiepunten", "studie", "campus", "bibliotheek",
+    "studentenvereniging", "studentenraad", "studentenbegeleiding", "studiebegeleiding",
+    "studieadvies", "studiekeuze", "studiebeurs", "studietoelage", "studievoorschot",
+    "studiecontract", "studiegeld", "studieprogramma", "studievoortgang", "verplichte vakken",
+    "keuzevakken", "studiebegeleider", "studiebegeleiders", "studieadviseur", "beurs",
+    "toelage", "voorschot", "contract", "geld", "programma", "voortgang", "puntentabel",
+    "puntensysteem", "puntentotaal", "puntentekort", "puntentekorten", "keuze", "begeleider",
+    "begeleiders", "adviseur", "bib", "vereniging", "raad", "begeleiding", "advies",
+    "collegegeld", "studievertraging", "studieduur", "studieschuld", "inschrijfprocedure",
+    "college", "colleges", "docent", "professor", "opleidingstraject", "curriculum", "ECTS",
+    "studieresultaten", "tentamen", "examenperiode", "studielening", "financiële steun",
+    "studiefinanciering", "studentenwoning", "studentenkamer", "kot", "huisvesting",
+    "studentenhuisvesting", "campusleven", "studiegenoot", "mentor", "studiecoach", "onderwijs",
+    "lesmateriaal", "scriptie", "proefschrift", "promotieonderzoek", "promotor", "studentenkaart",
+    "studentenpas", "introductiedag", "introductieweek", "studievereniging", "studieclub",
+    "werkcollege", "hoorcollege", "seminarie", "project", "stageplaats", "stagemogelijkheden",
+    "studieplanning", "zelfstudie", "studiegids", "studiehandleiding", "onderwijsmodule",
+    "onderwijsvorm", "curriculumonderdeel", "werkstuk", "studieprestatie", "studieopdracht",
+    "opdracht", "tentamenweek", "herkansing", "academisch", "academische kalender", "studiepuntenregistratie",
+    "onderzoeksproject", "onderzoeksvoorstel", "studieomgeving", "studieruimte", "collegezaal",
+    "studiebeursaanvraag", "beurzen", "studielast", "studiebelasting", "academische graad",
+    "bachelor", "master", "promotie", "promovendus", "wetenschappelijke publicatie", "scriptiebegeleider",
+    "scriptieonderwerp", "studievorm", "studierichting", "afstudeerrichting", "keuzevak",
+    "vak", "studievereisten", "studieplanning", "rooster", "collegejaar", "studietijd",
+    "zelfstudietijd", "onderwijsperiode", "studiehouding", "studielandschap", "studiestructuur",
+    "examenreglement", "examencommissie", "studieaanpak", "studiebegeleidingsplan", "onderwijsvisie",
+    "onderwijsfilosofie", "studentenzorg", "studietraject", "studiemethode", "onderwijsmethode",
+    "studentenleven", "studentenwelzijn", "studentenactiviteiten", "studentensport", "studielasturen",
+    "studiepuntensysteem", "studiejaar", "academische jaarindeling", "studie-uren", "zelfstudie-uren",
+    "academische gemeenschap", "academische integriteit", "academische ondersteuning", "afstudeerproject",
+    "anonieme cijfers", "begeleidingsgesprek", "bijles", "brochures", "campusfaciliteiten", "cijferlijst",
+    "collegerooster", "communicatievaardigheden", "contacturen", "courseware", "curriculummodules", 
+    "curriculumvernieuwing", "didactiek", "digitale leeromgeving", "docententeam", "dyslexiebegeleiding",
+    "e-learning", "educatieve programma's", "elektronische leeromgeving", "erasmusprogramma", "ethiek",
+    "evaluatie", "evaluatierapport", "feedback", "formatief assessment", "gastcollege", "goede studiehouding",
+    "groepsopdracht", "groepsproject", "herinschrijving", "huiswerkbegeleiding", "informatieavond",
+    "inschrijfdeadline", "inschrijfgeld", "interactief onderwijs", "introweek", "kansengelijkheid",
+    "kennistoets", "klas", "klasgenoten", "klaslokaal", "klassenmentor", "klasrooster", "leerdoelen",
+    "leerstof", "lesmethodes", "lestijden", "mediatheek", "mobiele leeromgeving", "nieuwsbrief",
+    "onderwijsbeleid", "onderwijsevaluatie", "onderwijskundige", "onderwijsmanagement", "onderwijsontwikkeling",
+    "onderwijsondersteuning", "onderwijspersoneel", "onderwijsprestaties", "onderwijsprogramma", "onderwijssystemen",
+    "onderwijstaken", "onderwijsuitvoering", "onderwijsvisie", "onderzoeksbegeleiding", "onderzoekscompetenties",
+    "onderzoeksvaardigheden", "online onderwijs", "opfriscursus", "opleidingscommissie", "opleidingsprofiel",
+    "praktijkonderwijs", "praktijkopdracht", "praktijkervaring", "projectbegeleiding", "projectonderwijs",
+    "projectvaardigheden", "reflectieverslag", "resultaatgericht", "scriptietraject", "studieachterstand",
+    "studiebegeleidingstraject", "studieboeken", "studiedag", "studiehouding", "studie-intensiteit",
+    "studieplanning", "studiepuntentabel", "studievereniging", "studievoorlichting", "tentamencijfers",
+    "tentamenresultaten", "theorie-examen", "toelatingsvoorwaarden", "toetsing", "verdiepingsmodule",
+    "verplichte literatuur", "videolectures", "werkcolleges", "werkplekleren", "zelfstudieopdrachten"
+    ]
+
     for keyword in keywords:
         if keyword.lower() in query.lower():
             return True
     return False
 
-def build_prompt(question, chat_history):
+def build_prompt(question):
     # Construct the prompt with the chat history
-    prompt = "You are a knowledgeable assistant specialized in providing information about Erasmushogeschool Brussel. Maintain context and respond accurately based on the conversation history. Here is the conversation history:\n"
-    for entry in chat_history:
-        role = entry['role']
-        content = entry['content']
-        prompt += f"{role}: {content}\n"
-    prompt += f"user: {question}\nassistant:"
-    return prompt
+    prompt = (
+        "Je bent een behulpzame assistent die vragen beantwoordt over de Erasmus Hogeschool Brussel (EhB)."
+        "Beantwoord alleen vragen die specifiek betrokken zijn met de erasmus hogeschool Brussel en zijn opleidingen."
+        "Als de vraag niet relevant is aan de erasmus hogeschool Brussel en zijn opleidingen, geef dan aan dat de vraag niet relevant is"
+        f"Vraag: {question}\nAntwoord: "
+    )
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('HTTP trigger function received a request.')
@@ -33,7 +85,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Extract the question and chat history from the request
         request_data = req.get_json()
         question = request_data.get('question')
-        chat_history = request_data.get('chat_history', [])
 
         if not question:
             return func.HttpResponse("Please pass a question in the request", status_code=400)
@@ -43,7 +94,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse("This chatbot only answers questions about Erasmushogeschool Brussel.", status_code=400)
 
         # Perform prompt engineering by building the prompt with chat history
-        prompt = build_prompt(question, chat_history)
+        prompt = build_prompt(question)
 
         # Send the prompt to the OpenAI deployment
         response = openai.Completion.create(
@@ -62,3 +113,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except Exception as e:
         logging.error(f"Error processing request: {e}")
         return func.HttpResponse("Error processing request", status_code=500)
+, 
